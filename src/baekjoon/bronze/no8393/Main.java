80a8f2f759a919ece2536d806e331242072eee05
choihongjun <vvclsbalam@gmail.com>
2025-07-12 17:56:52 +0900

    feat: no8393

---
+++ src/baekjoon/bronze/no8393/Main.java
@@ -0,0 +1,12 @@
+package baekjoon.bronze.no8393;
+
+import java.util.Scanner;
+
+public class Main {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = sc.nextInt();
+        System.out.println(n * (n + 1) / 2);
+    }
+}
