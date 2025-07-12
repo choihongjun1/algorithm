8344e4fa6411023a7d8cb44e11339b026a60b9dc
choihongjun <vvclsbalam@gmail.com>
2025-07-12 16:37:41 +0900

    refactor: no10171

--- src/baekjoon/bronze/no10171/Main.java
+++ src/baekjoon/bronze/no10171/Main.java
@@ -1,9 +1,6 @@
 package baekjoon.bronze.no10171;
 
-import java.util.Scanner;
-
 public class Main {
-    static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
         System.out.println("\\    /\\\n )  ( ')\n(  /  )\n \\(__)|");
