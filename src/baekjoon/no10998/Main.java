d34005e5450e6e59c351758e4809ddf03d8ca9e2
choihongjun <vvclsbalam@gmail.com>
2025-07-12 14:04:49 +0900

    feat: no10998

---
+++ src/baekjoon/no10998/Main.java
@@ -0,0 +1,14 @@
+package baekjoon.no10998;
+
+import java.util.Scanner;
+
+public class Main {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(a * b);
+    }
+}
