ce19db796b79bcf88d3bb3629d808bff7ff61c39
choihongjun <vvclsbalam@gmail.com>
2025-07-12 14:49:39 +0900

    feat: no10926

---
+++ src/baekjoon/no10926/Main.java
@@ -0,0 +1,12 @@
+package baekjoon.no10926;
+
+import java.util.Scanner;
+
+public class Main {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String id = sc.next();
+        System.out.println(id.concat("??!"));
+    }
+}
