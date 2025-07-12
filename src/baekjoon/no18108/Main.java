cf87af85f4438969697bf0e10815c5f244d7c492
choihongjun <vvclsbalam@gmail.com>
2025-07-12 14:53:50 +0900

    feat: no18108

---
+++ src/baekjoon/no18108/Main.java
@@ -0,0 +1,12 @@
+package baekjoon.no18108;
+
+import java.util.Scanner;
+
+public class Main {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int year = sc.nextInt();
+        System.out.println(year - 543);
+    }
+}
