46458ddedd4a6d0038b19c17202e05d7b7271a03
choihongjun <vvclsbalam@gmail.com>
2025-07-12 14:46:15 +0900

    feat: no10869

---
+++ src/baekjoon/no10869/Main.java
@@ -0,0 +1,21 @@
+package baekjoon.no10869;
+
+import java.util.Scanner;
+
+public class Main {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        if (b != 0) {
+            System.out.println(a / b);
+            System.out.println(a % b);
+        }
+
+    }
+}
+
