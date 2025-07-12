c06587c34dec452ecef3cdd8ad1fed0635280c7c
choihongjun <vvclsbalam@gmail.com>
2025-07-12 16:43:55 +0900

    feat: no1330

---
+++ src/baekjoon/bronze/no1330/Main.java
@@ -0,0 +1,16 @@
+package baekjoon.bronze.no1330;
+
+import java.util.Scanner;
+
+public class Main {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if(a > b) System.out.println(">");
+        else if (a < b) System.out.println("<");
+        else System.out.println("==");
+    }
+}
