0b3b09938db590aa45cd9f311c6fca67eabbedd6
choihongjun <vvclsbalam@gmail.com>
2025-07-12 16:27:32 +0900

    feat: no11382

---
+++ src/baekjoon/bronze/no11382/Main.java
@@ -0,0 +1,15 @@
+package baekjoon.bronze.no11382;
+
+import java.util.Scanner;
+
+public class Main {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+        long c = sc.nextLong();
+
+        System.out.println(a + b + c);
+    }
+}
