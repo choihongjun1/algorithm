ce9ea1ffe847cf6ad668bcbbd6d2339ddd62341c
choihongjun <vvclsbalam@gmail.com>
2025-07-16 22:16:23 +0900

    feat: no2748

---
+++ src/baekjoon/bronze/no2748/Main.java
@@ -0,0 +1,22 @@
+package baekjoon.bronze.no2748;
+
+import java.util.Scanner;
+
+public class Main {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = sc.nextInt();
+        long[] arr = new long[n+1];
+        if(n != 0) arr[1] = 1;
+
+        System.out.println(fibo(n, arr));
+    }
+
+    public static long fibo(int n, long[] arr) {
+        if(n == 0 || n == 1) return n;
+        if(arr[n] != 0) return arr[n];
+        arr[n] = fibo(n-1, arr) + fibo(n-2, arr);
+        return arr[n];
+    }
+}
