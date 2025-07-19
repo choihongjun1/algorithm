397564275c1bf7f09d03fa61a0f41888a4ebdf6e
choihongjun <vvclsbalam@gmail.com>
2025-07-19 14:54:41 +0900

    feat: no16430

---
+++ src/baekjoon/bronze/no16430/Main.java
@@ -0,0 +1,13 @@
+package baekjoon.bronze.no16430;
+
+import java.util.Scanner;
+
+public class Main {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println((b-a) + " " + b);
+    }
+}
