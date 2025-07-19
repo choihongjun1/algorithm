a29aca63df449f10d65aa5ac6d2268490a4ca93f
choihongjun <vvclsbalam@gmail.com>
2025-07-19 14:50:44 +0900

    feat: no10718

---
+++ src/baekjoon/bronze/no10718/Main.java
@@ -0,0 +1,12 @@
+package baekjoon.bronze.no10718;
+
+import java.util.Scanner;
+
+public class Main {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("강한친구 대한육군");
+        System.out.println("강한친구 대한육군");
+    }
+}
