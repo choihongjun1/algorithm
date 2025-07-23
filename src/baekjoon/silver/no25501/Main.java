d9cc057f9b3346f969ce0e0709dab1a669404022
choihongjun <vvclsbalam@gmail.com>
2025-07-23 20:23:34 +0900

    feat: no25501

---
+++ src/baekjoon/silver/no25501/Main.java
@@ -0,0 +1,30 @@
+package baekjoon.silver.no25501;
+
+import java.util.Scanner;
+
+public class Main {
+    static Scanner sc = new Scanner(System.in);
+    static int recursionCount = 0;
+
+    public static void main(String[] args) {
+        int t = sc.nextInt();
+        String[] result = new String[t];
+        for (int i = 0; i < t; i++) {
+            String str = sc.next();
+            result[i] = isPalindrome(str) + " " + recursionCount;
+            recursionCount = 0;
+        }
+        for(String s : result) System.out.println(s);
+    }
+
+    public static int isPalindrome(String s) {
+        return recursion(s, 0, s.length() - 1);
+    }
+
+    public static int recursion(String s, int l, int r) {
+        recursionCount++;
+        if(l >= r) return 1;
+        else if(s.charAt(l) != s.charAt(r)) return 0;
+        else return recursion(s, l+1, r-1);
+    }
+}
