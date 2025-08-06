3bff91c7f1df0a76f7f9a61e97e89602a49d899d
choihongjun <vvclsbalam@gmail.com>
2025-08-06 23:45:40 +0900

    feat: no21964

---
+++ src/baekjoon/bronze/no21964/Main.java
@@ -0,0 +1,12 @@
+package baekjoon.bronze.no21964;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String s = sc.next();
+        System.out.println(s.substring(n-5));
+    }
+}
