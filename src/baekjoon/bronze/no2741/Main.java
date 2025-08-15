c68c9cee608e6f3c1cbd4e825cab29c45e687504
choihongjun <vvclsbalam@gmail.com>
2025-08-15 14:09:37 +0900

    feat: no2741

---
+++ src/baekjoon/bronze/no2741/Main.java
@@ -0,0 +1,19 @@
+package baekjoon.bronze.no2741;
+
+import java.io.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter((new OutputStreamWriter(System.out)));
+
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+        for(int i = 1; i <= n; i++) {
+            bw.write(i + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
