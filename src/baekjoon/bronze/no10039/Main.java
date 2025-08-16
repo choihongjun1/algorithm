cf64c609498820c1244ad38cb2447933ea9fbcd1
choihongjun <vvclsbalam@gmail.com>
2025-08-16 13:56:49 +0900

    feat: no10039

---
+++ src/baekjoon/bronze/no10039/Main.java
@@ -0,0 +1,16 @@
+package baekjoon.bronze.no10039;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+        for(int i = 0; i < 5; i++) {
+            int score = sc.nextInt();
+            if(score > 40) sum += score;
+            else sum += 40;
+        }
+        System.out.println(sum / 5);
+    }
+}
