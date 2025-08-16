665c274200193e905ea6c0991fa651c07d1d3e73
choihongjun <vvclsbalam@gmail.com>
2025-08-16 13:47:13 +0900

    feat: no10699

---
+++ src/baekjoon/bronze/no10699/Main.java
@@ -0,0 +1,11 @@
+package baekjoon.bronze.no10699;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        SimpleDateFormat date = new SimpleDateFormat("yyyy-MM-dd");
+        System.out.println(date.format(new Date()));
+    }
+}
