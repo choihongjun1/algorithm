c5680708246f5cabc2a50429324d00d3ef88bed1
choihongjun <vvclsbalam@gmail.com>
2025-08-17 20:41:39 +0900

    feat: no9655

---
+++ src/baekjoon/silver/no9655/Main.java
@@ -0,0 +1,39 @@
+package baekjoon.silver.no9655;
+
+import java.io.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    static String[] winners;
+
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+        winners = new String[n];
+        if(n > 3) {
+            winners[1] = "SK";
+            winners[2] = "CY";
+            winners[3] = "SK";
+        }
+
+        bw.write(game(n, 1));
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    public static String game(int n, int turn) {
+        String[] players = {"CY", "SK"};
+        if(n <= 3) return players[n % 2];
+
+        if(winners[n-1] == null) winners[n-1] = game(n-1, (turn + 1) % 2);
+        if(winners[n-3] == null) winners[n-3] = game(n-3, (turn + 1) % 2);
+
+        String one = winners[n-1];
+        String three = winners[n-3];
+        if(one.equals(three)) return one.equals("SK") ? "CY" : "SK";
+        else return players[turn];
+    }
+}
