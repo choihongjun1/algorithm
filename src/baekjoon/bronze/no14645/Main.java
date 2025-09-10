a2e40b8452c88e103daf73df543b9b5a12838d0b
choihongjun <vvclsbalam@gmail.com>
2025-09-10 14:47:00 +0900

    feat: no14645

---
+++ src/baekjoon/bronze/no14645/Main.java
@@ -0,0 +1,16 @@
+package baekjoon.bronze.no14645;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        for(int i = 0; i < n; i++) {
+            sc.nextInt();
+            sc.nextInt();
+        }
+        System.out.println("비와이");
+    }
+}
