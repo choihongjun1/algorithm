8c6f2dc982b9509f36fa7edac769ff88dff75f9b
choihongjun <vvclsbalam@gmail.com>
2025-09-14 22:51:33 +0900

    feat: no24723

---
+++ src/baekjoon/bronze/no24723/Main.java
@@ -0,0 +1,11 @@
+package baekjoon.bronze.no24723;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(Math.round(Math.pow(2, n)));
+    }
+}
