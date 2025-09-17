3af09c995a379b141072c929e365f3cb2dc9735e
choihongjun <vvclsbalam@gmail.com>
2025-09-17 21:25:14 +0900

    feat: no13909

---
+++ src/baekjoon/silver/no13909/Main.java
@@ -0,0 +1,10 @@
+package baekjoon.silver.no13909;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println((int)Math.sqrt(sc.nextInt()));
+    }
+}
