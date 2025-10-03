1f2a75dc161002d6984dddd71b1593454eeea1f4
choihongjun <vvclsbalam@gmail.com>
2025-10-03 22:23:11 +0900

    feat: no27959

---
+++ src/baekjoon/bronze/no27959/Main.java
@@ -0,0 +1,10 @@
+package baekjoon.bronze.no27959;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextInt() * 100 >= sc.nextInt() ? "Yes" : "No");
+    }
+}
