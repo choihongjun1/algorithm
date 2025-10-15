a8ce92a2a74970e75a1ca6eabfb5febcbfe007e4
choihongjun <vvclsbalam@gmail.com>
2025-10-15 14:20:02 +0900

    feat: no10821

---
+++ src/baekjoon/bronze/no10821/Main.java
@@ -0,0 +1,11 @@
+package baekjoon.bronze.no10821;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] line = sc.next().split(",");
+        System.out.println(line.length);
+    }
+}
