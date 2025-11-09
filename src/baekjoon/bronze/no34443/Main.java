682abfa1686e9d2f53a09df609034fab12636405
choihongjun <vvclsbalam@gmail.com>
2025-11-09 11:37:31 +0900

    feat: no34443

---
+++ src/baekjoon/bronze/no34443/Main.java
@@ -0,0 +1,13 @@
+package baekjoon.bronze.no34443;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int c = sc.nextInt();
+        int p = sc.nextInt();
+        System.out.println(n * p);
+    }
+}
