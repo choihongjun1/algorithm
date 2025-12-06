d82527cbea2d495f950194bcbb64757a458d2a6d
choihongjun <vvclsbalam@gmail.com>
2025-12-06 14:16:28 +0900

    refactor: no20115

--- src/baekjoon/silver/no20115/Main.java
+++ src/baekjoon/silver/no20115/Main.java
@@ -1,22 +1,19 @@
 package baekjoon.silver.no20115;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] arr = new int[n];
+        int max = 0;
+        double drink = 0;
         for(int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
+            int num = sc.nextInt();
+            if(max < num) max = num;
+            drink += num / 2.0;
         }
-        Arrays.sort(arr);
-
-        double drink = arr[n-1];
-        for(int i = 0; i < n-1; i++) {
-            drink += arr[i] / 2.0;
-        }
+        drink += max / 2.0;
 
         System.out.println(drink);
     }
