6d488b43af0d0c279854d84113d8e07316ae81b6
choihongjun <vvclsbalam@gmail.com>
2025-12-13 13:55:22 +0900

    feat: no5565

---
+++ src/baekjoon/bronze/no5565/Main.java
@@ -0,0 +1,14 @@
+package baekjoon.bronze.no5565;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int total = sc.nextInt();
+        for(int i = 0; i < 9; i++) {
+            total -= sc.nextInt();
+        }
+        System.out.println(total);
+    }
+}
