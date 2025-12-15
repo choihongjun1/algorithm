040cb13effc9d76b024513b2fd40ad21925ad4f6
choihongjun <vvclsbalam@gmail.com>
2025-12-15 20:16:33 +0900

    feat: no10189

---
+++ src/baekjoon/bronze/no10189/Main.java
@@ -0,0 +1,11 @@
+package baekjoon.bronze.no10189;
+
+public class Main {
+    public static void main(String[] args) {
+        String s = "#  # #### #### #  #\n" +
+                "#### #  # #  # # #\n" +
+                "#### #  # #  # # #\n" +
+                "#  # #### #### #  #";
+        System.out.println(s);
+    }
+}
