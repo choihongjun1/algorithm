cdb2608510f1da398c44c27d951766f3c14aacc2
choihongjun <vvclsbalam@gmail.com>
2025-12-17 18:15:31 +0900

    feat: no5596

---
+++ src/baekjoon/bronze/no5596/Main.java
@@ -0,0 +1,18 @@
+package baekjoon.bronze.no5596;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int s = 0;
+        int t = 0;
+        for(int i = 0; i < 4; i++) {
+            s += sc.nextInt();
+        }
+        for(int i = 0; i < 4; i++) {
+            t += sc.nextInt();
+        }
+        System.out.println(Math.max(s, t));
+    }
+}
