1e4654ee0cc8c03b1956d0a9dcf5526eac4367ec
choihongjun <vvclsbalam@gmail.com>
2025-12-18 17:21:17 +0900

    feat: no1712

---
+++ src/baekjoon/bronze/no1712/Main.java
@@ -0,0 +1,19 @@
+package baekjoon.bronze.no1712;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        if(b >= c) {
+            System.out.println(-1);
+            return;
+        }
+
+        System.out.println(a / (c - b) + 1);
+    }
+}
