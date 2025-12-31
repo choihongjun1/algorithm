613b24ea6651592bba3631271042af86c6b1302c
choihongjun <vvclsbalam@gmail.com>
2025-12-31 17:26:50 +0900

    feat: no11931

---
+++ src/baekjoon/silver/no11931/Main.java
@@ -0,0 +1,59 @@
+package baekjoon.silver.no11931;
+
+import java.io.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        int[] temp = new int[n];
+        mergeSort(arr, temp, 0, arr.length-1);
+
+        for(int i : arr) bw.write(i + "\n");
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+
+    public static void mergeSort(int[] arr, int[] temp, int left, int right) {
+        if(left < right) {
+            int mid = (left + right) / 2;
+            mergeSort(arr, temp, left, mid);
+            mergeSort(arr, temp, mid+1, right);
+            merge(arr, temp, left, mid, right);
+        }
+    }
+
+    public static void merge(int[] arr, int[] temp, int left, int mid, int right) {
+        int i = left;
+        int j = mid + 1;
+        int k = left;
+
+        while (i <= mid && j <= right) {
+            if (arr[i] >= arr[j]) {
+                temp[k++] = arr[i++];
+            } else {
+                temp[k++] = arr[j++];
+            }
+        }
+
+        while (i <= mid) {
+            temp[k++] = arr[i++];
+        }
+
+        while (j <= right) {
+            temp[k++] = arr[j++];
+        }
+
+        for (int idx = left; idx <= right; idx++) {
+            arr[idx] = temp[idx];
+        }
+    }
+}
