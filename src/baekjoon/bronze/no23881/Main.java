46115b620fdda0c15063bffb789ea67ece53afd8
choihongjun <vvclsbalam@gmail.com>
2026-01-02 21:50:31 +0900

    feat: no23881

---
+++ src/baekjoon/bronze/no23881/Main.java
@@ -0,0 +1,49 @@
+package baekjoon.bronze.no23881;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static int k;
+    static int count = 0;
+
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        selectionSort(arr);
+        if(count != k) bw.write("-1");
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+
+    public static void selectionSort(int[] arr) throws IOException {
+        for(int i = arr.length-1; i > 0; i--) {
+            int max = 0;
+            for(int j = 1; j <= i; j++) {
+                if(arr[j] > arr[max]) max = j;
+            }
+
+            if(max != i) {
+                int temp = arr[i];
+                arr[i] = arr[max];
+                arr[max] = temp;
+                if(++count == k) {
+                    bw.write(arr[max] + " " + arr[i]);
+                    return;
+                }
+            }
+        }
+    }
+}
