050cce356e18816f1f6a85e751f5e73cf7560ae3
choihongjun <vvclsbalam@gmail.com>
2026-01-02 22:46:54 +0900

    feat: no23969

---
+++ src/baekjoon/bronze/no23969/Main.java
@@ -0,0 +1,49 @@
+package baekjoon.bronze.no23969;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static int k;
+    static int count = 0;
+
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        bubbleSort(arr);
+        if(count != k) bw.write("-1");
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+
+    public static void bubbleSort(int[] arr) throws IOException {
+        for(int i = arr.length-1; i > 0; i--) {
+            for(int j = 0; j < i; j++) {
+                if(arr[j] > arr[j+1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    if(++count == k) {
+                        for(int k = 0; k < arr.length-1; k++) {
+                            bw.write(arr[k] + " ");
+                        }
+                        bw.write(arr[arr.length-1] + "\n");
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
