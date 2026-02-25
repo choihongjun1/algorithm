6c0dca301ed92140c432a421b023b487bbf5de2c
choihongjun <vvclsbalam@gmail.com>
2026-02-25 23:51:38 +0900

    feat: no7891

---
+++ src/baekjoon/bronze/no7891/Main.java
@@ -0,0 +1,23 @@
+package baekjoon.bronze.no7891;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        int t = Integer.parseInt(br.readLine());
+        for(int i = 0; i < t; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            bw.write(x + y + "\n");
+        }
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
