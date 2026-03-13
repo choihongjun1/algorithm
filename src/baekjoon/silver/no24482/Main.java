77e9ec5163e2b65b8f3fb73d52531f8f88bf825e
choihongjun <vvclsbalam@gmail.com>
2026-03-13 23:21:41 +0900

    feat: no24482

---
+++ src/baekjoon/silver/no24482/Main.java
@@ -0,0 +1,57 @@
+package baekjoon.silver.no24482;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static List<Integer>[] adj;
+    static boolean[] visited;
+    static int[] depth;
+
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int node = Integer.parseInt(st.nextToken());
+        int edge = Integer.parseInt(st.nextToken());
+        int start = Integer.parseInt(st.nextToken());
+        adj = new ArrayList[node+1];
+        visited = new boolean[node+1];
+        depth = new int[node+1];
+        for(int i = 1; i <= node; i++) adj[i] = new ArrayList<>();
+
+        for(int i = 0; i < edge; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            adj[u].add(v);
+            adj[v].add(u);
+        }
+
+        for(int i = 1; i <= node; i++) {
+            adj[i].sort(Collections.reverseOrder());
+        }
+
+        dfs(start, 0);
+
+        for(int i = 1; i <= node; i++) {
+            if(visited[i]) bw.write(depth[i] + "\n");
+            else bw.write("-1\n");
+        }
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+
+    public static void dfs(int start, int d) {
+        visited[start] = true;
+        depth[start] = d;
+        for(int u : adj[start]) {
+            if(!visited[u]) dfs(u, d+1);
+        }
+    }
+}
