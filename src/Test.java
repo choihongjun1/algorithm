e9c240771f43188279f1337e9643d7637dd15091
choihongjun <vvclsbalam@gmail.com>
2026-03-13 20:56:12 +0900

    chore: 테스트 삭제

--- src/Test.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class Test {
-    static Scanner sc = new Scanner(System.in);
-
-    public static void main(String[] args) {
-        System.out.println(" ".repeat(0));
-    }
-}
