c4174498acfc2568b11848b4b6d1ab4136877417
choihongjun <vvclsbalam@gmail.com>
2026-04-05 16:23:53 +0900

    feat: no6131

---
+++ src/baekjoon/bronze/no6131/Main.java
@@ -0,0 +1,18 @@
+package baekjoon.bronze.no6131;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int count = 0;
+        for(int a = 1; a <= 500; a++) {
+            for(int b = 1; b <= a; b++) {
+                if(a * a - b * b == n) count++;
+                else if(a * a - b * b < n) break;
+            }
+        }
+        System.out.println(count);
+    }
+}
