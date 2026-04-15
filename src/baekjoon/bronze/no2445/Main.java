21348fe298b8d80718b8456cf5a9e5ca0c1071f9
choihongjun <vvclsbalam@gmail.com>
2026-04-15 10:20:59 +0900

    feat: no2445

---
+++ src/baekjoon/bronze/no2445/Main.java
@@ -0,0 +1,17 @@
+package baekjoon.bronze.no2445;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        for(int i = 1; i <= n; i++) {
+            System.out.println("*".repeat(i) + " ".repeat(2*n - 2*i) + "*".repeat(i));
+        }
+        for(int i = n-1; i >= 0; i--) {
+            System.out.println("*".repeat(i) + " ".repeat(2*n - 2*i) + "*".repeat(i));
+        }
+    }
+}
