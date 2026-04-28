54007b20e9fd0289668981be6e28224da393c290
choihongjun <vvclsbalam@gmail.com>
2026-04-28 13:04:06 +0900

    feat: no35515

---
+++ src/baekjoon/notratable/no35515/Main.java
@@ -0,0 +1,7 @@
+package baekjoon.notratable.no35515;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Good Bye, BOJ! 너무 아쉽습니다. 그동안 감사했습니다.");
+    }
+}
