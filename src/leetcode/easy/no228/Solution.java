da529dcb51741dd28a5350254ace37b7e7db2792
choihongjun <vvclsbalam@gmail.com>
2026-05-14 02:35:33 +0900

    feat: no228

---
+++ src/leetcode/easy/no228/Solution.java
@@ -0,0 +1,30 @@
+package leetcode.easy.no228;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        List<String> result = new ArrayList<>();
+        if(nums.length == 0) return result;
+
+        int start = nums[0];
+        int end = nums[0];
+        for(int i = 1; i < nums.length; i++) {
+            if(nums[i] - 1 == nums[i-1]) end = nums[i];
+            else {
+                if(start == end) {
+                    result.add(String.valueOf(start));
+                } else {
+                    result.add(start + "->" + end);
+                }
+                start = nums[i];
+                end = nums[i];
+            }
+        }
+        if(start == end) result.add(String.valueOf(start));
+        else result.add(start + "->" + end);
+
+        return result;
+    }
+}
