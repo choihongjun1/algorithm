fcefd055008310b87acb380358893e7080a7ebe0
choihongjun <vvclsbalam@gmail.com>
2026-05-26 21:00:04 +0900

    feat: no496

---
+++ src/leetcode/easy/no496/Solution.java
@@ -0,0 +1,28 @@
+package leetcode.easy.no496;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int[] result = new int[nums1.length];
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums2.length; i++) {
+            map.put(nums2[i], i);
+        }
+
+        for(int i = 0; i < nums1.length; i++) {
+            int index = map.get(nums1[i]);
+            int nextGreater = -1;
+            for(int j = index+1; j < nums2.length; j++) {
+                if(nums2[j] > nums1[i]) {
+                    nextGreater = nums2[j];
+                    break;
+                }
+            }
+            result[i] = nextGreater;
+        }
+
+        return result;
+    }
+}
