75148faf3a5f7cdacad36fc7c4a52b71887e6945
choihongjun <vvclsbalam@gmail.com>
2026-06-06 18:25:45 +0900

    feat: no867

---
+++ src/leetcode/easy/no867/Solution.java
@@ -0,0 +1,13 @@
+package leetcode.easy.no867;
+
+class Solution {
+    public int[][] transpose(int[][] matrix) {
+        int[][] transpose = new int[matrix[0].length][matrix.length];
+        for(int i = 0; i < transpose.length; i++) {
+            for(int j = 0; j < transpose[0].length; j++) {
+                transpose[i][j] = matrix[j][i];
+            }
+        }
+        return transpose;
+    }
+}
