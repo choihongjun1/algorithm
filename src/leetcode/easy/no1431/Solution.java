aea32d154ed3358e98cc19229adc9da9278e519f
choihongjun <vvclsbalam@gmail.com>
2026-06-08 16:47:12 +0900

    feat: no1431

---
+++ src/leetcode/easy/no1431/Solution.java
@@ -0,0 +1,21 @@
+package leetcode.easy.no1431;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int max = 0;
+        for(int i = 0; i < candies.length; i++) {
+            if(candies[i] > max) max = candies[i];
+        }
+
+        List<Boolean> result = new ArrayList<>();
+        for(int i = 0; i < candies.length; i++) {
+            if(candies[i] + extraCandies >= max) result.add(true);
+            else result.add(false);
+        }
+
+        return result;
+    }
+}
