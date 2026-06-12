e87f8c1e9920f9c0d4e5a5e89641550688fb7f5f
choihongjun <vvclsbalam@gmail.com>
2026-06-12 12:01:26 +0900

    feat: no1252

---
+++ src/leetcode/easy/no1252/Solution.java
@@ -0,0 +1,22 @@
+package leetcode.easy.no1252;
+
+class Solution {
+    public int oddCells(int m, int n, int[][] indices) {
+        int[] row = new int[m];
+        int[] col = new int[n];
+
+        for(int i = 0; i < indices.length; i++) {
+            row[indices[i][0]]++;
+            col[indices[i][1]]++;
+        }
+
+        int odd = 0;
+        for(int i = 0; i < row.length; i++) {
+            for(int j = 0; j < col.length; j++) {
+                if((row[i] + col[j]) % 2 == 1) odd++;
+            }
+        }
+
+        return odd;
+    }
+}
