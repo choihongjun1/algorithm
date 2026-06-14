475b3617e968c42d06ee0873c313fe9aad88a85b
choihongjun <vvclsbalam@gmail.com>
2026-06-14 13:50:25 +0900

    feat: no1486

---
+++ src/leetcode/easy/no1486/Solution.java
@@ -0,0 +1,12 @@
+package leetcode.easy.no1486;
+
+class Solution {
+    public int xorOperation(int n, int start) {
+        int result = start;
+        for(int i = 1; i < n; i++) {
+            int temp = start + 2 * i;
+            result = result ^ temp;
+        }
+        return result;
+    }
+}
