1b7c260c100c1e2a70f294845d0dee69eb7d1dfb
choihongjun <vvclsbalam@gmail.com>
2026-06-15 10:00:22 +0900

    feat: no1295

---
+++ src/leetcode/easy/no1295/Solution.java
@@ -0,0 +1,17 @@
+package leetcode.easy.no1295;
+
+class Solution {
+    public int findNumbers(int[] nums) {
+        int count = 0;
+        for(int i = 0; i < nums.length; i++) {
+            int n = nums[i];
+            int digit = 0;
+            while(n != 0) {
+                n /= 10;
+                digit++;
+            }
+            if(digit % 2 == 0) count++;
+        }
+        return count;
+    }
+}
