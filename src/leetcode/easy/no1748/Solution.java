b846da8b0dd637705b699ae34c82e4d226f447b6
choihongjun <vvclsbalam@gmail.com>
2026-06-24 23:15:47 +0900

    feat: no1748

---
+++ src/leetcode/easy/no1748/Solution.java
@@ -0,0 +1,16 @@
+package leetcode.easy.no1748;
+
+class Solution {
+    public int sumOfUnique(int[] nums) {
+        int[] count = new int[101];
+        for(int i = 0; i < nums.length; i++) {
+            count[nums[i]]++;
+        }
+
+        int sum = 0;
+        for(int i = 1; i < count.length; i++) {
+            if(count[i] == 1) sum += i;
+        }
+        return sum;
+    }
+}
