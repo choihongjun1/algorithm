276aebd273003b48b742f3b46614f3d61254ada5
choihongjun <vvclsbalam@gmail.com>
2026-07-06 22:04:21 +0900

    feat: no2656

---
+++ src/leetcode/easy/no2656/Solution.java
@@ -0,0 +1,11 @@
+package leetcode.easy.no2656;
+
+class Solution {
+    public int maximizeSum(int[] nums, int k) {
+        int max = 0;
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] > max) max = nums[i];
+        }
+        return k * (max + max + k - 1) / 2;
+    }
+}
