3eb60c81ac7f3fa78ef614d73c1c64234a06b1d0
choihongjun <vvclsbalam@gmail.com>
2026-07-09 22:29:35 +0900

    feat: no1344

---
+++ src/leetcode/easy/no1344/Solution.java
@@ -0,0 +1,11 @@
+package leetcode.easy.no1344;
+
+class Solution {
+    public double angleClock(int hour, int minutes) {
+        double hourAngle = hour * 30 + minutes / 2.0;
+        double minuteAngle = minutes * 6;
+        double angle = Math.max(hourAngle, minuteAngle) - Math.min(hourAngle, minuteAngle);
+        if(angle > 180) angle = 360 - angle;
+        return angle;
+    }
+}
