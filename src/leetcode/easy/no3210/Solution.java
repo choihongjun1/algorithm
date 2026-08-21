75075bb4d97506d4c741ed39a8a121fa46094b17
choihongjun <vvclsbalam@gmail.com>
2026-08-21 22:48:20 +0900

    feat: no3210

---
+++ src/leetcode/easy/no3210/Solution.java
@@ -0,0 +1,11 @@
+package leetcode.easy.no3210;
+
+class Solution {
+    public String getEncryptedString(String s, int k) {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < s.length(); i++) {
+            sb.append(s.charAt((i+k) % s.length()));
+        }
+        return sb.toString();
+    }
+}
