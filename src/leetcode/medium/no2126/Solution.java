2ca791468b94be77df28c3d58b35b9670d413926
choihongjun <vvclsbalam@gmail.com>
2026-08-25 21:31:09 +0900

    feat: no2126

---
+++ src/leetcode/medium/no2126/Solution.java
@@ -0,0 +1,15 @@
+package leetcode.medium.no2126;
+
+import java.util.Arrays;
+
+class Solution {
+    public boolean asteroidsDestroyed(int mass, int[] asteroids) {
+        Arrays.sort(asteroids);
+        long sum = mass;
+        for(int i = 0; i < asteroids.length; i++) {
+            if(sum < asteroids[i]) return false;
+            sum += asteroids[i];
+        }
+        return true;
+    }
+}
