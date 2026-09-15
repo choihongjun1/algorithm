2b5eaadf19d28be4966a5b4764f86cdc2e589c1c
choihongjun <vvclsbalam@gmail.com>
2026-09-16 01:55:31 +0900

    feat: no1464

---
+++ src/leetcode/easy/no1464/Solution.java
@@ -0,0 +1,10 @@
+package leetcode.easy.no1464;
+
+import java.util.Arrays;
+
+class Solution {
+    public int maxProduct(int[] nums) {
+        Arrays.sort(nums);
+        return (nums[nums.length-1]-1) * (nums[nums.length-2]-1);
+    }
+}
