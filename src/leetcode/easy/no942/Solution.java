80accbd84ba2a79448e8f476ff7b156d3033acc5
choihongjun <vvclsbalam@gmail.com>
2026-09-26 01:41:44 +0900

    feat: no942

---
+++ src/leetcode/easy/no942/Solution.java
@@ -0,0 +1,21 @@
+package leetcode.easy.no942;
+
+class Solution {
+    public int[] diStringMatch(String s) {
+        int n = s.length();
+        int[] perm = new int[n + 1];
+        int low = 0;
+        int high = n;
+
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == 'I') {
+                perm[i] = low++;
+            } else {
+                perm[i] = high--;
+            }
+        }
+        perm[n] = low;
+
+        return perm;
+    }
+}
